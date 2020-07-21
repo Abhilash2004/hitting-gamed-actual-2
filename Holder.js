4c1f0439ef2efad82b10399c72c5c90d29dc8b39
Abhilash C N <65819936+Abhilash2004@users.noreply.github.com>
2020-07-21 13:39:48 +0530

    Add files via upload

---
+++ Holder.js
@@ -0,0 +1,34 @@
+class Holder {
+    constructor(bodyA,pointB)
+    {
+    var options={
+    bodyA:bodyA,
+    pointB:pointB,
+    stiffness:0.04,
+    length:10
+    }
+    
+    this.pointB=pointB;
+
+    
+    this.holder=Constraint.create(options)
+    World.add(world,this.holder)
+    }
+    attach(body){
+        this.holder.bodyA=body;
+    }
+    fly (){
+        this.holder.bodyA=null   
+     }
+    
+    display() {
+        if(this.holder.bodyA){
+    var pointA =this.holder.bodyA.position
+    var pointB =this.pointB;
+   // strokeWeight(5)
+    line(pointA.x,pointA.y,pointB.x,pointB.y)
+    
+    }
+    
+    }
+}
